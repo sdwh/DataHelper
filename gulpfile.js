ed46207a1d31a060bee485ca9b4fcfc49982d42f
Webber Chang <631469@cpc365.onmicrosoft.com>
2020-12-11 19:59:44 +0800

    include gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const pkg = require('./package.json');
 const del = require('del');
+const ejs = require("gulp-ejs")
+const rename = require('gulp-rename')
+ 
+
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', async function() {
@@ -19,6 +23,12 @@ gulp.task('vendor', async function() {
       './node_modules/jquery/dist/jquery.min.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'))
+
+  // ejs
+  gulp.src('./src/*.ejs')
+    .pipe(ejs({ title: 'gulp-ejs' }))
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('./'))    
 
 })
 
